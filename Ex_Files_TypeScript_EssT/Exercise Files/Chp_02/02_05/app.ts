725c2da03a41e847c53ed35dcf6bc0d00cb2795c
Leanne Lacey-Byrne <llacey-byrne@saadian.com>
2019-09-25 21:16:00 +0100

    looping through arrays with for of

--- Ex_Files_TypeScript_EssT/Exercise Files/Chp_02/02_05/app.ts
+++ Ex_Files_TypeScript_EssT/Exercise Files/Chp_02/02_05/app.ts
@@ -7,3 +7,11 @@ var array = [
 for (var value of array) {
     console.log(value);
 }
+
+// for of works quicker & better than
+/*
+* for ( var index in array )
+* then you need to access each item via the idex:
+* var value = array[index];
+*
+ */
